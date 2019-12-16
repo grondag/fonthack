9b4dfdb6c3a753585ae5fdecb15cd3eaec95a679
Grondag <grondag@gmail.com>
2019-12-15 19:39:03 -0800

    Move fonts here

--- src/main/java/grondag/fonthack/FontHackClient.java
+++ src/main/java/grondag/fonthack/FontHackClient.java
@@ -3,7 +3,10 @@ package grondag.fonthack;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.util.Identifier;
+
 public class FontHackClient {
 	public static final String MOD_ID = "fonthack";
 	public static Logger LOG = LogManager.getLogger("Font Hack");
+	public static final Identifier READING_FONT = new Identifier("fonthack:reading");
 }
