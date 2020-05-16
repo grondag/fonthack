b51d828b2b7f09c103dc1a6c35ddd06231af9501
Grondag <grondag@gmail.com>
2020-05-15 20:54:09 -0700

    Vanilla oversampling is good enough

--- src/main/java/grondag/fonthack/font/NativeImageExt.java
+++
@@ -1,7 +0,0 @@
-package grondag.fonthack.font;
-
-public interface NativeImageExt {
-	void ext_setLuminanceAlpha(int u, int v, byte luminance, byte alpha);
-
-	int ext_getPackedLuminanceAlpha(int u, int v);
-}
